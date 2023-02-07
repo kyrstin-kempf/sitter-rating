50ba8f3398e58113ff01b2ff1c26059743288662
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-02-06 19:07:24 -0500

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,26 @@ Rails.application.routes.draw do
   
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
-  get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+  # get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
+
+  # resources :users, only: [:index, :show]
+  # resources :sitteres, only: [:index, :show]
+
+  post '/login', to: 'sessions#create'
+  get '/me', to: 'user#show'
+  delete '/logout', to: 'sessions#destroy'
+
+  get '/users', to: 'users#index'
+
+  get '/', to: 'sitters#index'
+  get '/sitters/:id', to: 'sitters#show'
+  post '/sitters', to: 'sitters#create'
+  patch '/sitters/:id', to: 'sitters#update'
+  delete '/sitters/:id', to: 'sitters#destroy'
+
+  get '/ratings', to: 'ratings#index'
+  post '/ratings', to: 'ratings#create'
+  patch '/ratings/:id', to: 'ratings#update'
+  # delete '/ratings/:id', to: 'ratings#destroy'
+
 end
