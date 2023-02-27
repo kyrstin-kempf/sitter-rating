19af6a495e60e576a99b393cf144ae70dfe8a566
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-02-27 13:24:51 -0500

    remove star rating

--- client/src/pages/OneSitter.js
+++ client/src/pages/OneSitter.js
@@ -1,4 +1,3 @@
-import starRating from '../assets/Five_Stars.png'
 import { useParams} from "react-router-dom";
 
 function OneSitter({ sitters }) {
@@ -13,9 +12,6 @@ function OneSitter({ sitters }) {
                     <h2>{sitter.first_name} {sitter.last_name}</h2>
                     <p>Experience: {sitter.years_of_experience} years</p>
                     <p>Hourly rate: ${sitter.hourly_rate}</p>
-                    <span>
-                    <img className='rating-stars' src={starRating} alt='average rating'/> | <p># ratings</p>
-                    </span>
                         {(
                             sitter.ratings.map((r) => (
                                 <div key={r.id} className="review-container">
--- client/src/pages/SitterList.js
+++ client/src/pages/SitterList.js
@@ -1,13 +1,6 @@
 import { Link } from 'react-router-dom';
-import starRating from '../assets/Five_Stars.png'
-import { useNavigate } from 'react-router-dom';
-const navigate = useNavigate();
 
 function SitterList({ sitters }) {
-
-    function handleAddRating() {
-        navigate('/');
-    }
 
     return (
         <div className="all-sitters-container">
@@ -17,13 +10,12 @@ function SitterList({ sitters }) {
                         <h2>{sitter.first_name} {sitter.last_name}</h2>
                         <p>Experience: {sitter.years_of_experience} years</p>
                         <p>Hourly rate: ${sitter.hourly_rate}</p>
-                        <span>
-                        <img className='rating-stars' src={starRating} alt='average rating'/> | <p># ratings</p>
-                        </span>
                         <Link to={`/sitters/${sitter.id}`}>
                         <p>See all reviews ›</p>
                         </Link>
-                        <button className='add-rating' onClick={handleAddRating}>+ Add Rating</button>
+                        <Link to={`/ratings/new`}>
+                        <p className='add-rating'>+ Add Rating</p>
+                        </Link>
                     </div>
                 ))
             ) : (
