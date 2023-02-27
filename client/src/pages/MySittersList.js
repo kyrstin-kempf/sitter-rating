ce37e187cfd24604e265c0619e2960aef3d064f4
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-02-27 16:22:14 -0500

    pass down user to component

--- client/src/pages/MySittersList.js
+++ client/src/pages/MySittersList.js
@@ -1,29 +1,40 @@
-// import { Link } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
-function MySittersList() {
+function MySittersList({ user, sitters }) {
+    
+    // const sitter_name = user.ratings.map((r) => (
+    //     <div key={r.id}>
+    //         <p>{r.sitter_id}</p>
+    //     </div>
+    // ))
+    // console.log(user.sitters)
+    // const sitter = sitters.find(s => JSON.stringify(s.id) === id)
 
     return (
         <div className="all-sitters-container">
             <h1>My Sitters</h1>
-            {/* {sitters.length > 0 ? (
-                sitters.map((sitter) => (
-                    <div key={sitter.id} className="sitter-box">
-                        <h2>{sitter.first_name} {sitter.last_name}</h2>
-                        <p>Experience: {sitter.years_of_experience} years</p>
-                        <p>Hourly rate: ${sitter.hourly_rate}</p>
-                        <Link to={`/sitters/${sitter.id}`}>
-                        <p>See all reviews ›</p>
-                        </Link>
-                        <Link to={`/ratings/new`}>
-                        <p className='add-rating'>+ Add Rating</p>
-                        </Link>
+            {sitters.length > 0 ? (
+                user.sitters.map((s) => (
+                    <div key={s.id} className="sitter-box">
+                        <h2>{s.first_name} {s.last_name}</h2>
+                        <p>Experience: {s.years_of_experience} years</p>
+                        <p>Hourly rate: ${s.hourly_rate}</p>
+                        {(
+                            s.ratings.map((r) => (
+                                <div key={r.id} className="review-container">
+                                    <p>{r.rating}</p>
+                                    <p>{r.review}</p>
+                                    <p>{r.user_id}</p>
+                                </div>
+                            ))
+                        )}
                     </div>
                 ))
             ) : (
                 <div>
                     <h2>no sitters found</h2>
                 </div>
-            )} */}
+            )}
         </div>
     );
 }
