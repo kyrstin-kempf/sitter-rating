0b0ea5b0002bca08aa9369c1587405f3140c2a1a
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-02-28 11:18:18 -0500

    change what is shown in json

--- app/serializers/rating_serializer.rb
+++ app/serializers/rating_serializer.rb
@@ -1,6 +1,6 @@
 class RatingSerializer < ActiveModel::Serializer
   attributes :id, :rating, :review, :sitter_id, :user_id
 
-  belongs_to :sitter
-  belongs_to :client
+  # belongs_to :sitter
+  # belongs_to :user
 end
