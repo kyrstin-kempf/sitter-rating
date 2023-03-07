3fe61a9443247e99e15640903078bbb88c0ce60e
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-03-06 23:29:01 -0500

    add and delete sitter from state

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -36,24 +36,74 @@ function App() {
   }
 
   const addSitterRating = (rating) => {
-    const s = sitters.find(s => s.id === rating.sitter_id)
-    // console.log(s);
-    const newS = {...s, ratings: [...s.ratings, rating]}
-    const newSitters = sitters.map((sit) => sit.id === s.id ? newS : sit)
-    setSitters(newSitters)
-  }
+    // console.log(rating)
+    const s = sitters.find(s => s.id === rating.sitter_id);
+    const newS = {...s, ratings: [...s.ratings, rating]};
+    const newSitters = sitters.map((sit) => sit.id === s.id ? newS : sit);
+    
+    let updatedUser = { ...user };
+    const existingSitter = updatedUser.sitters.find(s => s.id === rating.sitter_id);
+
+    if(!existingSitter) {
+      updatedUser = {
+        ...updatedUser,
+        sitters: [...updatedUser.sitters, {
+          id: s.id, 
+          first_name: s.first_name,
+          last_name: s.last_name,
+          email: s.email,
+          hourly_rate: s.hourly_rate,
+          years_of_experience: s.years_of_experience,
+          ratings: [rating] }],
+      };
+    } else {
+      const updatedRatings = [...existingSitter.ratings, rating];
+      const updatedSitters = updatedUser.sitters.map(s => {
+        if(s.id === rating.sitter_id) {
+          return { ...s, ratings: updatedRatings };
+        }
+        return s;
+      });
+      updatedUser = { ...updatedUser, sitters: updatedSitters };
+    }
+    
+    setSitters(newSitters);
+    setUser(updatedUser);
+
+    // --- add sitter if new sitter and rating. Only add rating of already have sitter
+    // -------------- both states need to updated
+  };
 
   const updateSitterRating = (updatedRating) => {
     const s = sitters.find(s => s.id === updatedRating.sitter_id)
     // console.log(s);
     const sitRate = s.ratings.map(r => {
       if(r.id === updatedRating.id) {
-        return updatedRating
+        return updatedRating 
       } return r
     })
     const newS = {...s, ratings: sitRate}
     const newSitters = sitters.map((sit) => sit.id === s.id ? newS : sit)
     setSitters(newSitters)
+  }
+
+  function deleteSitterRating(ratingId, sitterId) {
+    const s = sitters.find(s => s.id.toString() === sitterId)
+    const sitterRating = s.ratings.filter(r => r.id.toString() !== ratingId)
+    const newS = {...s, ratings: sitterRating}
+    const newSitters = sitters.map((sit) => sit.id === s.id ? newS : sit)
+    
+    let updatedUser = { ...user };
+    const existingSitter = updatedUser.sitters.find(ss => JSON.stringify(ss.id) === sitterId);
+    
+    if(existingSitter && existingSitter.ratings.length === 1) {
+      const updatedSitters = updatedUser.sitters.filter(s => JSON.stringify(s.id) !== sitterId);
+      console.log(updatedSitters)
+      updatedUser = { ...updatedUser, sitters: updatedSitters};
+    }
+    
+    setSitters(newSitters);
+    setUser(updatedUser)
   }
 
   if (!user) return <Login onLogin={setUser} />;
@@ -63,11 +113,11 @@ function App() {
         <BrowserRouter>
           <NavBar setUser={setUser} />
           <Routes>
-            <Route path='/' element={<MySittersList user={user} sitters={sitters} />} />
+            <Route exact path='/' element={<MySittersList user={user} sitters={sitters} setSitters={setSitters} />} />
             <Route path='/sitters' element={<SitterList sitters={sitters} />} />
             <Route path='/sitters/new' element={<NewSitter addSitter={addSitter} />} />
             <Route path='/sitters/:id' element={<OneSitter sitters={sitters} user={user} addSitterRating={addSitterRating} />} />
-            <Route path='/sitters/:sitter_id/ratings/:id/edit' element={<UpdateRating sitters={sitters} updateSitterRating={updateSitterRating} />} />
+            <Route path='/sitters/:sitter_id/ratings/:id/edit' element={<UpdateRating sitters={sitters} updateSitterRating={updateSitterRating} deleteSitterRating={deleteSitterRating} />} />
           </Routes>
         </BrowserRouter>
       </div>
