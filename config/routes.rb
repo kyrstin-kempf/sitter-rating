4aeffba45a1fa366cb2001050d4ab8b073999bd7
Kyrstin Kempf <kyrstin.kempf@gmail.com>
2023-03-06 23:30:44 -0500

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -10,23 +10,17 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  # resources :sitters, only: [:index, :show, :create, :update, :destroy]
+ 
   get '/sitters', to: 'sitters#index'
   get '/sitters/:id', to: 'sitters#show'
   post '/sitters', to: 'sitters#create'
-  # delete '/sitters/:id', to: 'sitters#destroy'
   
-  # resources :ratings
-  # get '/ratings', to: 'ratings#index'
+
   post '/ratings', to: 'ratings#create'
   patch '/ratings/:id', to: 'ratings#update'
   delete '/ratings/:id', to: 'ratings#destroy'
 
 end
 
-# landing page is list my sitters not all sitters
-# not my sitter until I review them - adding sitter is general adding
-# add star to each review, nix avg
-# add sitter errors
 
  
